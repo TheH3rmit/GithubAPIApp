a51f705c80195d8a794af49b7eadf386855b33f6
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2025-04-01 20:24:37 +0200

    Initial

---
+++ src/main/java/org/example/githubapiapp/GithubApiAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubapiapp/GithubApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
